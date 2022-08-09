126c244e2a7d0ad92ac63652f7cde0ee7b463723
Iam maria theresa <iam.mariatheresaelkazzi@gmail.com>
2022-08-09 20:33:20 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+// const cards = ["Guadalupe", "Ollie", "Aki"];
+// console.log(cards);
+// writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function writeCards(cards, event) {
+    let arr = [];
+    for (let i = 0; i < cards.length; i++) {
+        let newCards;
+        newCards = `Thank you, ${cards[i]}, for the wonderful ${event} gift!`;
+        arr.push(newCards);
+    }
+    // console.log(arr.length, "birthday");
+    return arr;
+}
+// console.log(writeCards(cards, "birthday"));
+function countDown(number) {
+    for (; number >= 0; number--) {
+        console.log(number);
+    }
+}
+countDown(10);
